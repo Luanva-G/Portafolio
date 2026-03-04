0bd76528385f0eb78898bfaf2bb9d25c11cd1f57
Luanva <luisangelaguilarvaldovinos@gmail.com>
2026-03-03 19:43:08 -0600

    Fix: limpieza de importaciones y bootstrap arreglado

--- mi-portafolio/src/main.tsx
+++ mi-portafolio/src/main.tsx
@@ -1,23 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 import './assets/css/LineIcons.2.0.css'; 
 import './assets/css/animate.css';
-import './assets/css/bootstrap-5.0.0-beta1.min.css';
 import './assets/css/glightbox.min.css';
 import './assets/css/tiny-slider.css';
 import './assets/css/lindy-uikit.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
     <main>
        <App />
-
-
     </main>
   </StrictMode>,
 )
