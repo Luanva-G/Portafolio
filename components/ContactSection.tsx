529c7072ff78582558dba052f774c0500fdc5c34
Luanva <luisangelaguilarvaldovinos@gmail.com>
2026-03-04 21:53:30 -0600

    fix: re-estructuración final en la raíz y limpieza de sourcemaps

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState} from 'react';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 const ContactSection: React.FC = () => {
--- components/Preloader.tsx
+++ components/Preloader.tsx
@@ -1,5 +1,5 @@
-import react, { use } from 'react';
-import React, { useState, useEffect } from 'react';
+//import react, { use } from 'react';
+import { useState, useEffect } from 'react';
 import type { ReactElement } from 'react'; 
 
 
